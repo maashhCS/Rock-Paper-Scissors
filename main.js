3cf660e1c00c48a712994fe13ebd83e0d39cff6c
maashh <58293726+maashhCS@users.noreply.github.com>
2023-03-28 00:37:28 +0200

    added function getPlayerChoice and playRound

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 'use strict'
+
+function getPlayerChoice(){
+    let valid = false;
+    while(valid = false){
+    let playerChoice = prompt('').toLowerCase;
+        if(playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors' ) {
+            valid = true;
+            return playerChoice;
+        } else {
+            console.log('You entered a wrong move. Try again!');
+        }
+    }
+}
+
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * (3 - 1 + 1) + 1);
     if(randomNumber === 1){
@@ -9,3 +23,22 @@ function getComputerChoice(){
         return 'Scissors';
     }
 }
+
+function playRound(computerChoice, playerChoice){
+    if(computerChoice === playerChoice){
+        return "It's a Draw!";
+    } else if(computerChoice === 'rock' && playerChoice === 'paper'){
+        return "You Win! Paper beats Rock";
+    } else if(computerChoice === 'scissors' && playerChoice === 'rock'){
+        return "You Win! Rock beats Scissors";
+    } else if(computerChoice === 'paper' && playerChoice === 'scissors'){
+        return "You Win! Scissors beats Paper";
+    } else if(computerChoice === 'paper' && playerChoice === 'rock'){
+        return "You Lose! Paper beats Rock";
+    } else if(computerChoice === 'rock' && playerChoice === 'scissors'){
+        return "You Lose! Rock beats Scissors";
+    } else if(computerChoice === 'scissors' && playerChoice === 'paper'){
+        return "You Lose! Scissors beats Paper";
+    } 
+}
+
